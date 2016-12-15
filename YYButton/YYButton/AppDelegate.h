b0d8c37573adc28a35a41e9f26ba8ad2086591b7
Younger <mac@MacdeMacBook-Air.local>
2016-12-15 19:25:41 +0800

    commit

---
+++ YYButton/YYButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYButton
+//
+//  Created by Mac on 2016/12/15.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYButton/YYButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYButton
+//
+//  Created by Mac on 2016/12/15.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
